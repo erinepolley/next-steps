3f29c2219fbf69682f7719c11a1d16aa27ea8b56
Erin Polley <erinpolley@bitgo.com>
2025-04-14 16:10:26 -0500

    create snapshots of Ash resources with Extension Migrations: No extensions to install
    Generating Tenant Migrations:
    
    Generating Migrations:
    No changes detected, so no migrations or snapshots have been created. and Getting extensions in current project...
    Running migration for AshPostgres.DataLayer...
    
    16:10:26.749 [info] Migrations already up

--- lib/next_steps/repo.ex
+++ lib/next_steps/repo.ex
@@ -1,5 +1,9 @@
 defmodule NextSteps.Repo do
-  use Ecto.Repo,
-    otp_app: :next_steps,
-    adapter: Ecto.Adapters.Postgres
+  use AshPostgres.Repo, otp_app: :next_steps
+
+  # This is required for migrations.
+  # https://hexdocs.pm/ash_postgres/AshPostgres.Repo.html#module-installed-extensions
+  def installed_extensions do
+    ~w[ash-functions citext]
+  end
 end
